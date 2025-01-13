1c6c8650aa4373c763dbaf5af79e00810e8a4efa
Muhammad Abdullah <ma2404374@gmail.com>
2025-01-13 23:27:11 +0500

    some changes for request and response

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import os
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from typing import Union, Optional
+from typing import Union, Optional, List
 from fastapi.params import Body
 from pydantic import BaseModel
 import psycopg
 from psycopg.rows import dict_row
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -22,12 +22,6 @@ user = os.getenv("user")
 database = os.getenv("dbname")
 password = os.getenv("password")
 host = os.getenv("host")
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 
 try:
@@ -66,24 +60,18 @@ async def root():
     return {"message": "Hello, World!"}
 
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    post = db.query(models.Post).all()
-    return {"data": post}
-
-
 # Getting all posts from database
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
 # Insert a new post into the database using POST method
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(
     #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #     (post.title, post.content, post.published),
@@ -94,11 +82,11 @@ def create_post(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"body": new_post}
+    return new_post
 
 
 # Getting single post from database with it's id using get method
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (id,))
     # post = cursor.fetchone()
@@ -108,7 +96,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: '{id}' not found",
         )
-    return {"data": post}
+    return post
 
 
 # Delete a post using delete method with post id
@@ -131,8 +119,10 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 # updating a post by using put method with post id
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(
+    id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)
+):
     # cursor.execute(
     #     """UPDATE posts SET title = %s, content = %s , published = %s WHERE id = %s RETURNING *""",
     #     (post.title, post.content, post.published, id),
@@ -149,4 +139,4 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
