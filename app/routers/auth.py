390c9b8e0a1bd4d360068d40472704844d9b36d3
Muhammad Abdullah <ma2404374@gmail.com>
2025-01-25 00:04:06 +0500

    successfully created access token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter, Response, Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
     user = (
         db.query(models.User)
-        .filter(models.User.email == user_credentials.email)
+        .filter(models.User.email == user_credentials.username)
         .first()
     )
     if not user:
@@ -23,4 +27,5 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
 
     # create a token
     # return the token
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
